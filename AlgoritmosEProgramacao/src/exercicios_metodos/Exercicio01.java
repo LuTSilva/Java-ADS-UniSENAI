61f1a7ea629e0f388208e85153e5650502d19bad
Luan <luantome.sc@gmail.com>
2025-05-27 23:38:07 -0300

    adicionado atividades de modularização

---
+++ AlgoritmosEProgramacao/src/exercicios_metodos/Exercicio01.java
@@ -0,0 +1,56 @@
+package exercicios_metodos;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+	public static void main(String[] args) {
+		// 1 - Calculadora Simples: Implemente métodos para as 4 operações básicas e um
+		// menu onde o usuário pode escolher qual operação deseja realizar até que saia
+		// da aplicação.
+
+		Scanner sc = new Scanner(System.in);
+		int opcao;
+		
+		do {
+			opcao = menu(sc);
+			switch(opcao) {
+			case 1 -> System.out.printf("Resultado: %.2f%n", soma(lerDouble("Numero 1: ", sc), lerDouble("Numero 2: ", sc)));
+			case 2 -> System.out.printf("Resultado: %.2f%n", subtracao(lerDouble("Numero 1: ", sc), lerDouble("Numero 2: ", sc)));
+			case 3 -> System.out.printf("Resultado: %.2f%n", multiplicacao(lerDouble("Numero 1: ", sc), lerDouble("Numero 2: ", sc)));
+			case 4 -> System.out.printf("Resultado: %.2f%n", divisao(lerDouble("Numero 1: ", sc), lerDouble("Numero 2: ", sc)));
+			default -> System.out.println("Opção Inválida!");
+			}
+		} while (opcao !=5);
+		sc.close();
+	}
+
+	public static double soma(double a, double b) {
+		return a + b;
+	}
+
+	public static double subtracao(double a, double b) {
+		return a - b;
+	}
+
+	public static double multiplicacao(double a, double b) {
+		return a * b;
+	}
+
+	public static double divisao(double a, double b) {
+		return (double) a / b;
+	}
+
+	public static int menu(Scanner sc) {
+		System.out.println("Escolha uma operação: 1 - Soma \n"
+				+ "2 - Subtração \n"
+				+ "3 - Multiplicação \n"
+				+ "4 - Divisão \n"
+				+ "5 - Sair \n");
+		return sc.nextInt();
+	}
+	
+	public static double lerDouble(String msg, Scanner sc) {
+		System.out.print(msg);
+		return sc.nextDouble();
+	}
+}
---
+++ AlgoritmosEProgramacao/src/exercicios_metodos/Exercicio02.java
@@ -0,0 +1,49 @@
+package exercicios_metodos;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        //  2 - Em uma competição, as pessoas são organizadas por categorias, 
+        // as quais, são determinadas pela idade: 
+        // 5 a 7 anos - Infantil A
+        // 8 a 10 anos - Infantil B
+        // 11-17 anos - Juvenil
+        // Maiores de 18 anos (inclusive) - Adulto
+        // Construa um programa para cadastrar nome e idade de N pessoas (somente o 
+        // nome e a idade serão informados), e mostre o nome e a categoria
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = lerInt("Quantas pessoas deseja cadastrar? ", sc);
+        String resultado = "";
+        for (int i = 0; i < n; i++) {
+            String nome = lerString("Informe o nome: ", sc);
+            int idade = lerInt("Idade de " + nome + ": ", sc);
+            resultado += "\n" + nome + " - " + verificaCategoria(idade);
+        }
+
+        sc.close();
+        
+    }
+    public static String verificaCategoria(int idade) {
+        if (idade < 5) return "Idade inválida";
+        if (idade <= 7) return "Infantil A";
+        if (idade <= 10) return "Infantil B";
+        if (idade <= 17) return "Juvenil";
+        return "Adulto";
+    }
+
+    public static int lerInt(String msg, Scanner sc) {
+        System.out.print(msg);
+        return sc.nextInt();
+    }
+    public static void exibeMsg(String msg) {
+        System.out.println(msg);
+    }
+    
+    public static String lerString(String msg, Scanner sc) {
+        System.out.print(msg);
+        return sc.nextLine();
+    }
+}
